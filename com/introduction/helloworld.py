00fd6c737b152b31a4ff49d7393fa47000789111
Burak TAN <btan@netas.lab.nortel.com>
2016-06-10 09:08:06 +0300

    logout eklendi

--- com/introduction/helloworld.py
+++ com/introduction/helloworld.py
@@ -16,9 +16,8 @@ import time
 driver = webdriver.Firefox()
 driver.get("http://kariyer.net")
 driver.find_element_by_xpath(".//*[@id='Header']/div[2]/div[2]/div/div[1]/a[2]").click()
-driver.implicitly_wait(3)
-driver.find_element_by_id("lgnUserName").send_keys("your_username")
-driver.find_element_by_id("lgnPassword").send_keys("your_password")
+driver.find_element_by_id("lgnUserName").send_keys("username")
+driver.find_element_by_id("lgnPassword").send_keys("password")
 driver.find_element_by_id("LinkButton1").click()
 
 
@@ -32,6 +31,13 @@ driver.find_element_by_xpath(".//*[@id='dvOzgecmis']/ul/li[1]/span[4]/div/ul/li[
 time.sleep(3)
 driver.find_element_by_xpath(".//*[@id='A2']/span").click()
 time.sleep(3)
+
+logout = driver.find_element_by_xpath(".//*[@id='lnkAdayIsimSoyIsim']")
+ActionChains(driver).move_to_element(logout).perform()
+driver.find_element_by_xpath(".//*[@id='A2']").click()
+
+
+
 
 #print(driver.current_url)
 #driver.get("http://gmail.com")
